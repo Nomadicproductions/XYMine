a7c159a48b7a0d449cab58765adb3f8cb592bae5
joeyaugust1 <joeyaugust83@gmail.com>
2025-09-23 17:23:57 +1000

    Increase player's jump power from 7 to 10

--- script.js
+++ script.js
@@ -1007,7 +1007,7 @@ function drawTriggerOverlay() {
 
 const player = {
   x: 0, y: 0, width: tileSize * 0.8, height: tileSize * 0.8,
-  color: '#00f', dx: 0, dy: 0, speed: 2.5, jumpPower: 7,
+  color: '#00f', dx: 0, dy: 0, speed: 2.5, jumpPower: 10,
   grounded: false, coinCount: 0, jumpsRemaining: 2
 };
 
